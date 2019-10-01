d516a32a845d64e56188fae107814308c8fc7667
TharinduMD <1994mdtr@gmail.com>
2019-10-01 21:07:10 +0530

    add setter & getter to the Id

--- src/main/java/entities/tharindu.java
+++ src/main/java/entities/tharindu.java
@@ -6,15 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class ${Entity} {
+public class tharindu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-	public long getId(){
-		return id;
-	}
-	public void setId(long id){
-		this.id = id;
-	}
 }
